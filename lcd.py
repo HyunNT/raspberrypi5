9cd8355ef531b9d7325081ba24c52a544a360ff0
HyunNT <157787911+HyunNT@users.noreply.github.com>
2024-01-26 16:50:23 +0900

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-import LCD1602  # Import module for interfacing with LCD1602
-import time     # Import module for timing functions
+import LCD1602
+import time    
 
 def setup():
     # Initialize LCD with I2C address 0x27 and enable backlight
     LCD1602.init(0x27, 1)
     # Display the message 'Greetings!' at the top-left corner (row 0, column 0)
-    LCD1602.write(0, 0, 'Greetings!')
+    LCD1602.write(1, 0, 'HI Devicemart!')
     # Display the message 'From SunFounder' on the second line (row 1, column 1)
-    LCD1602.write(1, 1, 'From SunFounder')
+    LCD1602.write(2, 1, 'I2C LCD1602!')
     time.sleep(2)  # Display messages for 2 seconds
 
 try:
